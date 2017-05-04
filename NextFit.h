23aca8bfa21c4b8e9f1dd00118d3b3805d86c2e5
SimonJ-PC\SimonJ9 <simonj142857@gmail.com>
2017-05-03 23:54:26 -0400

    no message

--- NextFit.h
+++ NextFit.h
@@ -11,6 +11,7 @@ void Sim_Next_Fit(struct process_list* pl, FILE* output)
     unsigned int sim_time = 0;
     char frames[FRAME_SIZE];
     unsigned int remained = FRAME_SIZE;
+    unsigned int counter = 0;
     
     unsigned int i, j, frame_ind, frame_counter;
     for(i = 0; i < FRAME_SIZE; i++)
@@ -25,16 +26,21 @@ void Sim_Next_Fit(struct process_list* pl, FILE* output)
     plist->cap = pl->cap;
     plist->_index = pl->_index;
     plist->list = (struct process*)realloc(plist->list, sizeof(struct process) * pl->cap);
+    counter = pl->_size;
     
     
     for(i = 0; i < pl->_size; i++)
     {
         add_process(plist, pl->list[i]);
+        if(pl->list[i].t_arrival_2 > 0)
+        {
+            counter++;
+        }
     }
     /*algo*/
     printf("time %dms: Simulator started (Contiguous -- Next-Fit)\n", sim_time);
     
-    while(plist->_size > 0)
+    while(counter > 0)
     {
         for(i = 0; i < plist->_size; i++)
         {
@@ -106,15 +112,13 @@ void Sim_Next_Fit(struct process_list* pl, FILE* output)
                     }
                     if(frame_counter < plist->list[i]._mem)
                     {
-                        
-                        printf("COUNTER: %d\n", frame_counter);
-                        printf("REMAIN: %d\n", remained);
                         //defrag
                         
                         fprintf(output, "time %dms: Cannot place process %c -- starting defragmentation\n", 
                             sim_time, plist->list[i].id);
                         
                         unsigned int dtime = 0;
+                        
                         unsigned int newid, oldid, charid;
                         char moved[BUFFER_SIZE];
                         charid = 0;
@@ -189,7 +193,7 @@ void Sim_Next_Fit(struct process_list* pl, FILE* output)
                         
                         fprintf(output, "time %dms: Placed process %c:\n", 
                             sim_time, plist->list[i].id);
-                        printf("frame_ind: %d\n", frame_ind);
+                        fflush(NULL);
                         print_frames(output, frames);
                         frame_ind = 0;
                         
@@ -209,24 +213,21 @@ void Sim_Next_Fit(struct process_list* pl, FILE* output)
                     if(frames[j - 1] == plist->list[i].id)
                     {
                         frames[j - 1] = '.';
-                        remained++;
                         frame_ind = j - 1;
+                        remained++;
+                        
                     }
                 }
                 fprintf(output, "time %dms: Process %c removed:\n", 
                     sim_time, plist->list[i].id);
                 print_frames(output, frames);
-                
-                
-                
-                
-                    
+                counter--;
             }
             
             /*finished proceee*/
-            if((sim_time >= plist->list[i].t_running_2 + plist->list[i].t_arrival_2 && 
+            if((sim_time == plist->list[i].t_running_2 + plist->list[i].t_arrival_2 && 
                 sim_time > 0) || 
-                (sim_time >= plist->list[i].t_running_1 + plist->list[i].t_arrival_1 &&
+                (sim_time == plist->list[i].t_running_1 + plist->list[i].t_arrival_1 &&
                 plist->list[i].t_running_2 == 0))
             {
                 remove_process(plist, plist->list[i].id);
@@ -250,4 +251,5 @@ void Sim_Next_Fit(struct process_list* pl, FILE* output)
     
     
     printf("time %dms: Simulator ended (Contiguous -- Next-Fit)\n", sim_time-1);
+    fflush(NULL);
 }
